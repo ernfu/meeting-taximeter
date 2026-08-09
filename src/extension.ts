e20ca51b36849fb77145c16c80a3000b25567649
ernfu <ernest@prophet.ml>
2026-08-09 16:38:00 +1000

    feat: add start/ stop meter

--- src/extension.ts
+++ src/extension.ts
@@ -32,26 +32,56 @@ export function meetingCostPerSecond(meeting: Meeting): number{
   return total;
 }
 
+export function calcElapsedSeconds(start: Date, now: Date): number{
+  // getTime in ms, /1000 to get seconds
+  return (now.getTime() - start.getTime()) / 1000
+}
+
+export function meter(meeting: Meeting, start: Date, now: Date): string{
+
+  const elapsedSeconds = calcElapsedSeconds(start, now)
+
+  const costPerSecond = meetingCostPerSecond(meeting)
+
+  return `meeting now costs ${costPerSecond * elapsedSeconds}`;
+}
+
+let startTime: Date | undefined;
+let meeting: Meeting | undefined;
+let startMeter: NodeJS.Timeout | undefined;
+
 
 export function activate(context: vscode.ExtensionContext){
+  const start = vscode.commands.registerCommand("meetingTaximeter.start", () => {
+    console.log("start command ran!");
+    vscode.window.showInformationMessage("Fuck. meetings. again.");
 
-    const start = vscode.commands.registerCommand("meetingTaximeter.start", () => {
-        console.log("command ran!");
-        vscode.window.showInformationMessage("Fuck. meetings. again.");
+    const testMeeting: Meeting = {
+      roles: [
+        {role: "ceo", count: 1, annualSalary: 400000}
+      ],
+      durationMinutes: 60
+    };
 
-        const testMeeting: Meeting = {
-          roles: [
-            {role: "ceo", count: 1, annualSalary: 400000}
-          ],
-          durationMinutes: 60
-        };
+    const testMeetCost = meetingCostPerSecond(testMeeting) * 60 * testMeeting.durationMinutes;
+    console.log(`1h of ceo's time is worth ${testMeetCost}`);
 
-        const testMeetCost = meetingCostPerSecond(testMeeting) * 60 * testMeeting.durationMinutes;
-        console.log(`1h of ceo's time is worth ${testMeetCost}`)
+    startTime = new Date();
+    meeting = testMeeting;
+    startMeter = setInterval(
+      () => {
+        if (!meeting || !startTime) return;
+        console.log(meter(meeting, startTime, new Date()))},
+      1000
+    );
+  });
+  context.subscriptions.push(start);
 
-      });
-      context.subscriptions.push(start);
-
+  const stop = vscode.commands.registerCommand("meetingTaximeter.stop", () => {
+      console.log("stop command ran!")
+      clearInterval(startMeter)
+  });
+  context.subscriptions.push(stop);
 }
 
 export function deactivate() {}
